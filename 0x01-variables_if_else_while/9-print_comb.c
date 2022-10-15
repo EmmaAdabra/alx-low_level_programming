6f00609cd48deba94256cb3326e785fa93064cca
EmmaAdabra <adabraemma@gmail.com>
2022-10-14 22:16:35 -0700

    fixing white space error on tasks 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-*main-program entry point.
-*Return:0 success, non zero error.
+ *main-program entry point.
+ *Return:0 success, non zero error.
 */
 int main(void)
 {
 	int num;
 
-	for (n/um = '0'; num <= '9'; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 		if (num == '9')
