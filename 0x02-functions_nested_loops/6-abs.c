5d18042e6be086f1c6cea8a6c3761e0fb6c02e88
EmmaAdabra <adabraemma@gmail.com>
2022-10-19 00:58:06 -0700

    solution to task 6 and 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of any given integer
+ * @n: the inputed argument should be of type integer
+ * Return: abs of the inputed value
+ **/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a given number
+ * @n: the value type of n is int
+ * Return: the last digit of a number
+ **/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
