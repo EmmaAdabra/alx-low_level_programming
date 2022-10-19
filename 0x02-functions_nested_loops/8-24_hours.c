a66a286335f43d51f1ef468d2fed12cc5f3a9bfa
EmmaAdabra <adabraemma@gmail.com>
2022-10-19 07:22:49 -0700

    olution to task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minutes in a day 00:00
+ * Return: void function
+ **/
+
+void jack_bauer(void)
+{
+	int count = 0;
+	int min;
+	int hr;
+
+	while (count <= 23)
+	{
+		for (min <= 59)
+		{
+			if (min != 59)
+			{
+				_putchar((hr / 10) + '0');
+				_putchar((hr % 10) + '0');
+			}
+			else
+			{
+				hr++;
+				_putchar(((hr - 1) / 10) + '0');
+				_putchar(((hr - 1) % 10) + '0');
+			}
+
+			_puchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		{
+	{	count++;
+}
