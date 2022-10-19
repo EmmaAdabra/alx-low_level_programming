9c167952cb9a72d53e6585ce21f7c2d0b254ff37
EmmaAdabra <adabraemma@gmail.com>
2022-10-18 23:46:36 -0700

    solution to task 4 and 5

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha -check if an inputed char is lower or upper case
+ * @c: char c is the inputed argument
+ * Return: 1 if arg is lower or upper and 0 if otherwise
+ **/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "maiin.h"
+
+/**
+ * print_sign - checks if an integer is +ve, -ve or 0
+ * @n: int are the required arg type
+ * Return: 0 for int == 0, 1 for +ve int and -1 for -ve int
+ **/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
+}
