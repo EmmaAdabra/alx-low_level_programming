675eed1feba85c8561eaffe70f8a351a31acdf8c
EmmaAdabra <adabraemma@gmail.com>
2022-10-21 15:35:41 -0700

    task 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 - 9
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n + '0');
+	}	n++;
+	_putchar('\n');
+}
