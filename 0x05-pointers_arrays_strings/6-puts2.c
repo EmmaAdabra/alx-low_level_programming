abe29a899e150ef98a599f1c6bf5376a85a9e4d5
EmmaAdabra <adabraemma@gmail.com>
2022-10-25 14:30:21 -0700

    errors

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 
 	while (str[len] != '\0')
 		len++;
-	for (index = 0; index < len; index += 2)
+	for (index = 0; index <= (len - 1); index += 2)
 	{
 		_putchar(str[index]);
 	}
