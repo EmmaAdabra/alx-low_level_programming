df81f4137f20d76c594455f1fa539086f76808cb
EmmaAdabra <adabraemma@gmail.com>
2022-10-27 18:51:51 -0700

    fixing erro

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
  * Return: A pointer to the destination string @dest.
  */
 
-char *strcat(char *dest, const char *src)
+char *strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
@@ -18,6 +18,6 @@ char *strcat(char *dest, const char *src)
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-
+	dest[dest_len++] = '\0';
 	return (dest);
 }
