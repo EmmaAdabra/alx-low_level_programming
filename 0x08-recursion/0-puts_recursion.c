39998e9f2673fdfb8095465da1e4ac4e4fa4068d
EmmaAdabra <adabraemma@gmail.com>
2022-11-03 10:22:46 -0700

    solution task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - output passed strings to screen
+ * @s: passed strings
+**/
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory contain programs fuctions writen in c to carry out similar or distinctive tasks, all implemented with recurtion concept.
