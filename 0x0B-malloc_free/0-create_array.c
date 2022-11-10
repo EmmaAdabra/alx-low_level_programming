cb3e43769f904a18e5fdf5818809644f1c2c12cd
EmmaAdabra <adabraemma@gmail.com>
2022-11-09 20:28:19 -0800

    first commit malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * *create_array - creates array of chars
+  * @size: specifies  array size
+  * @c: Char to initialise the array with
+  * Return: pointer to the array or NULL if unsuccesful
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,8 @@
+This Directory Contains Task from ALX programming school
+
+Programs is this directory are tasks used to implement Dynamic memory allocation in c
+
+Mainly implemented with the function
+
+malloc() - to assign mermory in heap during program execution
+free() - tp dealocate memory after used
