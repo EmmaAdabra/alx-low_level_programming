dbb6297e51b18a72a8b73edc6c9adde7dc36f9f8
EmmaAdabra <adabraemma@gmail.com>
2022-11-10 19:30:09 -0800

    debugging task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,41 +10,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index, index2 = 0;
-	char *buffer;
-	int indx;
-	unsigned int len = 0;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
-
 		s1 = "";
 
 	if (s2 == NULL)
 
 		s2 = "";
 
-	for (indx = 0; s1[indx]; indx++)
+	for (index = 0; s1[index]; index++)
 		len++;
 
-	buffer = malloc(sizeof(char) * (len + n));
-
-	if (buffer == NULL)
+	concat = malloc(sizeof(char) * (len + 1));
+	if (concat == NULL)
 		return (NULL);
 
-	for (index = 0; index < len + n; index++)
-	{
-		if (index < len)
-		{
-			buffer[index] = s1[index];
-		}
-		else
-		{
-			buffer[index] = s2[index2];
-			index2++;
-		}
-	}
+	len = 0;
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-	buffer[index] = '\0';
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
-	return (buffer);
+	concat[len] = '\0';
+
+	return (concat);
+
 }
