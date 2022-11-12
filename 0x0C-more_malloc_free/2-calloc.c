de24cb5a4c824e406750162eafe4d8644c5885ea
EmmaAdabra <adabraemma@gmail.com>
2022-11-11 20:05:16 -0800

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+/**
+ * _calloc - returns a pointer to an array memory
+ * @nmemb: get the number of element to be stored in  the array
+ * @size: get the size of the array in memeory
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *buffer;
+	char *_memset;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	buffer = malloc(size * nmemb);
+
+	if (buffer == NULL)
+		return (NULL);
+
+	buffer = _memset;
+	for (index = 0; index < (size * nmemb); index++)
+		_memset[index] = '\0';
+
+	return (buffer);
+
+}
