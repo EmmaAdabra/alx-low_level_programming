2923d1576e49164bb153b000ae97fc995d25b1eb
EmmaAdabra <adabraemma@gmail.com>
2022-11-14 14:34:23 -0800

    using predefine macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Display the name of the file the program is stored
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("%S\n", __FILE__);
+
+	return (0);
+}
