2e6b6f2f29ebe8c2ce07489e94994d8930a12155
EmmaAdabra <adabraemma@gmail.com>
2022-11-16 09:02:02 -0800

    betty style error fixed

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
+/**
+  * free_dog - free all the allocated memory to dog_t dog
+  * @d: a parameter of typedef dog_t (structure data type)
+  * Return: void
+  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
