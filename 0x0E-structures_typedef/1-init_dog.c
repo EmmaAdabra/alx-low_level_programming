15c7d3b1aa021326951ed263eca9898f61b47619
EmmaAdabra <adabraemma@gmail.com>
2022-11-15 20:46:49 -0800

    fixing bug in task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,14 @@
   * @owner: pointer to string
   * Return: void
   */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
+
 	if (d == 0)
 		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
