b7f7486209126036b32a2e99a6758401a637ffe5
EmmaAdabra <adabraemma@gmail.com>
2022-11-15 20:39:37 -0800

    debugging

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib>
 
 /**
   * init_dog - Initialize values for struct dog
@@ -11,7 +10,7 @@
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d == 0)
 		return;
 	d->name = name;
 	d->age = age;
