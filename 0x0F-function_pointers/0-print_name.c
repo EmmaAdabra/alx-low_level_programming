aef37262e20dc877ed7af807f745545422cd0254
EmmaAdabra <adabraemma@gmail.com>
2022-11-16 21:14:34 -0800

    error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 
+
 /**
   * print_name - print the name passed into it arguments
   * @name: a string pointer
@@ -8,7 +9,7 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	 if (name == NULL && f == NULL)
 		return;
 
 	f(name);
