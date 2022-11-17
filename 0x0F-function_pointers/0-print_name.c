b4480ad811aeccb14cb6936dfaa253b889ffd514
EmmaAdabra <adabraemma@gmail.com>
2022-11-16 21:27:28 -0800

    error not fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,14 @@
 #include "function_pointers.h"
 
-
 /**
-  * print_name - print the name passed into it arguments
-  * @name: a string pointer
-  * @f: a pointer to a function
-  * Return: void
-  */
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	 if (name == NULL)
+	if (name == NULL || f == NULL)
 		return;
-	 else if(f == NULL)
-		 return;
-	 else
-		 f(name);
+
+	f(name);
 }
